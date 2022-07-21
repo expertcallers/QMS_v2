88307d6bcab66153949ac76b20542fe60fad5384
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-07-21 18:52:03 +0530

    Corrections

--- Qms_v3/qms_app/views.py
+++ Qms_v3/qms_app/views.py
@@ -2582,7 +2582,8 @@ def CampaignAgentReportView(request):
                                        campaign_id=campaign, emp_id=emp_id,)
             else:
                 obj = i.objects.filter(campaign_id=campaign, emp_id=emp_id)
-            audits.append(obj)
+            if obj:
+                audits.append(obj)
         print(audits,'audits')
         type = "campaign"
         data = {"audit": audits, "type": type, "qa_list": qa_list, "agent_list": agent_list, "mgr_list": mgr_list}
